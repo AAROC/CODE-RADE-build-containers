386bed4b3aab7beb812cb8b63a043f0439d34a0b
Bruce Becker <brucellino@gmail.com>
2016-12-19 15:55:53 +0100

    added modules path to volumes

--- build-and-push.sh
+++ build-and-push.sh
@@ -11,3 +11,5 @@ for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   docker tag code-rade-build-containers-build-slave:latest quay.io/aaroc/code-rade-build-containers-build-slave:${base}
   docker push quay.io/aaroc/code-rade-build-containers-build-slave:${base}
 done
+echo "pushing data container"
+docker push quay.io
