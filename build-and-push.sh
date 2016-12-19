f5abf5711f686dbd2c4e090df5f0b5eb3aeb8f2a
Bruce Becker <brucellino@gmail.com>
2016-12-19 14:48:55 +0100

    Added variables for using Quay

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 # Script to build and tag all images
+#
+# Log into quay
+echo $QUAY_USER
+echo $QUAY_PASS
+docker login -p $QUAY_PASS -u $QUAY_USER quay.io
 for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   echo "Building container for ${base}"
   ansible-container --var-file vars-${base}.yml build
