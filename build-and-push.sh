22859dbc9e539d23c075986ce5048b0d89f22785
Bruce Becker <brucellino@gmail.com>
2016-12-22 23:24:52 +0100

    running containers in detached mode too

--- build-and-push.sh
+++ build-and-push.sh
@@ -8,6 +8,8 @@ docker login -p $QUAY_PASS -u $QUAY_USER quay.io
 for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   echo "Building container for ${base}"
   ansible-container --var-file vars-${base}.yml build
+  echo "running container for ${base}"
+  ansible-container --var-file vars-${base}.yml run -d
   docker tag code-rade-build-containers-build-slave:latest quay.io/aaroc/code-rade-build-containers-build-slave:${base}
   docker push quay.io/aaroc/code-rade-build-containers-build-slave:${base}
 done
