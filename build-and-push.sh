5ec0fac08e7fb2f510176c9bf5287836b90440e6
Bruce Becker <brucellino@gmail.com>
2016-12-26 12:41:15 +0100

    Updated to use a build cluster

--- build-and-push.sh
+++ build-and-push.sh
@@ -8,8 +8,8 @@ docker login -p $QUAY_PASS -u $QUAY_USER quay.io
 for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   echo "Building container for ${base}"
   ansible-container --var-file vars-${base}.yml build
+  ansible-container --var-file vars-${base}.yml run -d
   echo "running container for ${base}"
   docker tag code-rade-build-containers-build-slave:latest quay.io/aaroc/code-rade-build-containers-build-slave:${base}
-  ansible-container --var-file vars-${base}.yml run -d
-  docker push quay.io/aaroc/code-rade-build-containers-build-slave:${base}
+  #docker push quay.io/aaroc/code-rade-build-containers-build-slave:${base}
 done
