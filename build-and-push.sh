2f2027721685d568b74eb4163fe8eb7837322f6c
Bruce Becker <brucellino@gmail.com>
2016-12-28 20:14:50 +0100

    Not running containers

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,7 +6,7 @@ echo $QUAY_USER
 echo $QUAY_PASS
 docker login -p $QUAY_PASS -u $QUAY_USER quay.io
 ansible-container --var-file vars.yml build
-ansible-container --var-file vars.yml run -d
+#ansible-container --var-file vars.yml run -d
 echo "running cluster"
 for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   docker tag code-rade-build-containers-build-slave-${base}:latest quay.io/aaroc/code-rade-build-containers-build-slave-${base}:latest
