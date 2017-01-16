0463010a712679aa619499756ff21b2798d02c5f
Bruce Becker <brucellino@gmail.com>
2017-01-16 13:34:48 +0100

    Rename task for tagging cluster

--- build-and-push.sh
+++ build-and-push.sh
@@ -8,7 +8,7 @@ docker login -p $QUAY_PASS -u $QUAY_USER quay.io
 ansible-container --var-file vars.yml build
 ansible-container --var-file vars.yml push --push-to quay
 #ansible-container --var-file vars.yml run -d
-echo "running cluster"
+echo "tagging cluster"
 for base in centos6 centos7 ubuntu1404 ubuntu1610  ; do
   echo "Tagging code-rade-build-containers-build-slave-${base}:latest"
   docker tag code-rade-build-containers-build-slave-${base}:latest quay.io/aaroc/code-rade-build-containers-build-slave-${base}:latest
